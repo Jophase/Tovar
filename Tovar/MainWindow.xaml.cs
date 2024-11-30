9c59ecbe263cd95ba7ab3b5e48a6df4fdc45f9fc
Пегас <@DESKTOP-8MRGA7V>
2024-11-30 12:00:18 +0300

    Добавьте файлы проекта.

---
+++ Tovar/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿// MainWindow.xaml.cs
+using System.Collections.Generic;
+using System.Windows;
+
+namespace OnlineStore
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadProducts_Click(object sender, RoutedEventArgs e)
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product("Ноутбук", 999.99m, "Ноутбук 2025 года ,ультра Max-ProVersions"),
+                new Product("Смартфон", 499.99m, "Последние слово технологии"),
+                new Product("чай", 299.99m, "Для работы ума полезно, особенно утром в 5 часов")
+            };
+
+            ProductListBox.Items.Clear();
+
+            foreach (var product in products)
+            {
+                // Деконструкция объекта Product
+                var (name, price, description) = product;
+                ProductListBox.Items.Add($"{name} - {price:C} - {description}");
+            }
+        }
+    }
+}
+
---
+++ Tovar/Product.cs
@@ -0,0 +1,28 @@
+﻿// Product.cs
+using System;
+
+namespace OnlineStore
+{
+    public class Product
+    {
+        public string Name { get; }
+        public decimal Price { get; }
+        public string Description { get; }
+
+        // Конструктор с параметрами
+        public Product(string name, decimal price, string description)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+        }
+
+        // Деконструктор
+        public void Deconstruct(out string name, out decimal price, out string description)
+        {
+            name = Name;
+            price = Price;
+            description = Description;
+        }
+    }
+}
